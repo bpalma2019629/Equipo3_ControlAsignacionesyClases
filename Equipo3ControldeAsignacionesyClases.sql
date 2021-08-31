bd86dbd633ff26a38a4e945354206d686ef1a5ea
Kenneth Garcia <kgarcia-2020050@kinal.edu.gt>
2021-08-30 22:10:23 -0600

    Actualizando archivos

--- Equipo3ControldeAsignacionesyClases.sql
+++ Equipo3ControldeAsignacionesyClases.sql
@@ -39,7 +39,28 @@ insert into salon (capacidad,descripcion,nombre_salon)
 values(35,"Salon de Quimica","A-0002");
 
 insert into salon (capacidad,descripcion,nombre_salon)
-values(35,"Salon de Matematica","A-0003");
+values(40,"Salon de Matematica","A-0003");
+
+insert into salon (capacidad,descripcion,nombre_salon)
+values(50,"Salon de Musica","A-0004");
+
+insert into salon (capacidad,descripcion,nombre_salon)
+values(40,"Salon de Fisica Fundamental","A-0005");
+
+insert into salon (capacidad,descripcion,nombre_salon)
+values(35,"Salon de Ingles","A-0006");
+
+insert into salon (capacidad,descripcion,nombre_salon)
+values(40,"Salon de Computacion","A-0007");
+
+insert into salon (capacidad,descripcion,nombre_salon)
+values(50,"Salon de Pintura","A-0008");
+
+insert into salon (capacidad,descripcion,nombre_salon)
+values(90,"Salon de Teatro","A-0009");
+
+insert into salon (capacidad,descripcion,nombre_salon)
+values(25,"Direccion","A-0010");
 
 
 
